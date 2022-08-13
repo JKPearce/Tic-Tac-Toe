4ac15f8cf15c658374ceb0cba38499f2cc6c5683
Jaydon <pearcejaydon@gmail.com>
2022-08-13 20:08:39 +1000

    Create board and dummy input

---
+++ script.js
@@ -0,0 +1,12 @@
+//create gameBoard MODULE
+const gameBoard = (() => {
+    const makeBoard = () => {
+        //append
+    }
+
+    const clear = () => {
+        
+    }
+
+    return {makeBoard,}
+})();// it has brackets at the end making it an IIFE (Immediately Invoked Function Expression).
